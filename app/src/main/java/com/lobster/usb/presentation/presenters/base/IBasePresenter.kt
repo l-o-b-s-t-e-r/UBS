87b90e67b6b6e0467f72e270896006f75db02c0c
Vlad <vladislav-loboda@mail.ru>
2019-02-03 16:31:44 +0100

    created base Fragment + Presenter

---
+++ app/src/main/java/com/lobster/usb/presentation/presenters/base/IBasePresenter.kt
@@ -0,0 +1,25 @@
+package com.lobster.usb.presentation.presenters.base
+
+interface IBasePresenter {
+
+    interface View {
+
+    }
+
+    open class Actions<T : IBasePresenter.View> {
+        var view: T? = null
+
+        fun bindView(view: T) {
+            this.view = view
+        }
+
+        fun unbindView() {
+            this.view = null
+        }
+
+        fun dispose() {
+
+        }
+    }
+
+}
---
+++ app/src/main/java/com/lobster/usb/presentation/ui/base/BaseFragment.kt
@@ -0,0 +1,41 @@
+package com.lobster.usb.presentation.ui.base
+
+import android.content.Context
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lobster.usb.presentation.presenters.base.IBasePresenter
+
+import javax.inject.Inject
+
+abstract class BaseFragment<V : IBasePresenter.View, P : IBasePresenter.Actions<V>> : Fragment(),
+    IBasePresenter.View {
+
+    @Inject
+    lateinit var presenter: P
+
+    abstract fun inject()
+
+    abstract fun layoutId(): Int
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        inject()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) =
+        inflater.inflate(layoutId(), container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.bindView(this as V)
+    }
+
+    override fun onDestroyView() {
+        presenter.unbindView()
+        presenter.dispose()
+        super.onDestroyView()
+    }
+}
