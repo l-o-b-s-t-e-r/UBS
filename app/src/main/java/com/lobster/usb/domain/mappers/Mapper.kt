ceb6dbdaf50ffd0fe7ed49ca9887d9b18bf92695
Vlad <vladislav-loboda@mail.ru>
2019-02-06 01:18:07 +0100

    created Mapper

---
+++ app/src/main/java/com/lobster/usb/domain/mappers/Mapper.kt
@@ -0,0 +1,54 @@
+package com.lobster.usb.domain.mappers
+
+import com.lobster.usb.domain.dto.SymbolCodeDto
+import com.lobster.usb.domain.dto.SymbolDto
+import com.lobster.usb.domain.entities.SymbolCodeEntity
+import com.lobster.usb.domain.entities.SymbolEntity
+import com.lobster.usb.domain.pojo.Symbol
+
+object Mapper {
+
+    fun symbolCodesToEntity(symbolCodes: List<SymbolCodeDto>): List<SymbolCodeEntity> {
+        val symbolCodeEntities = mutableListOf<SymbolCodeEntity>()
+        symbolCodes.forEach {
+            symbolCodeEntities.add(symbolCodeToEntity(it))
+        }
+
+        return symbolCodeEntities
+    }
+
+    fun symbolCodeToEntity(symbolCodeDto: SymbolCodeDto): SymbolCodeEntity {
+        return SymbolCodeEntity(symbolCodeDto.symbol)
+    }
+
+    fun symbolsToEntity(symbolCodes: List<SymbolCodeDto>): List<SymbolCodeEntity> {
+        val symbolCodeEntities = mutableListOf<SymbolCodeEntity>()
+        symbolCodes.forEach {
+            symbolCodeEntities.add(symbolCodeToEntity(it))
+        }
+
+        return symbolCodeEntities
+    }
+
+    fun symbolToEntity(symbolDto: SymbolDto): SymbolEntity {
+        return SymbolEntity(symbolDto.change, symbolDto.lastPrice)
+    }
+
+    fun symbolEntitiesToSymbols(symbolEntities: List<SymbolEntity>): List<Symbol> {
+        val symbols = mutableListOf<Symbol>()
+        symbolEntities.forEach {
+            symbols.add(
+                Symbol(
+                    it.id,
+                    it.code!!.target.code,
+                    it.company!!.target.companyName,
+                    it.change,
+                    it.lastPrice,
+                    it.isFavorite
+                )
+            )
+        }
+
+        return symbols
+    }
+}
