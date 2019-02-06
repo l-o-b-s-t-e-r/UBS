6d763b114c03fcf25ea63f4766d631c0ec7c758f
Vlad <vladislav-loboda@mail.ru>
2019-02-06 01:22:40 +0100

    added base methods to LocalRepository (save/get)

--- app/src/main/java/com/lobster/usb/data/LocalRepository.kt
+++ app/src/main/java/com/lobster/usb/data/LocalRepository.kt
@@ -1,8 +1,74 @@
 package com.lobster.usb.data
 
+import com.lobster.usb.domain.entities.*
 import com.lobster.usb.domain.interfaces.ILocalRepository
+import com.lobster.usb.domain.mappers.Mapper
+import com.lobster.usb.domain.wrappers.SymbolsWrapper
+import io.objectbox.Box
 import io.objectbox.BoxStore
+import io.reactivex.Single
 
 class LocalRepository(private val boxStore: BoxStore) : ILocalRepository {
 
+    var boxSymbol: Box<SymbolEntity> = boxStore.boxFor(SymbolEntity::class.java)
+    var boxSymbolCode: Box<SymbolCodeEntity> = boxStore.boxFor(SymbolCodeEntity::class.java)
+    var boxSymbolCompany: Box<SymbolCompanyEntity> = boxStore.boxFor(SymbolCompanyEntity::class.java)
+    var boxSymbolNews: Box<SymbolNewsEntity> = boxStore.boxFor(SymbolNewsEntity::class.java)
+
+    override fun saveSymbolCodes(symbolCodes: List<SymbolCodeEntity>): Single<List<SymbolCodeEntity>> {
+        return Single.fromCallable {
+            boxSymbolCode.put(symbolCodes)
+            boxSymbolCode.all
+        }
+    }
+
+    override fun getSymbolCodes(query: String, page: Int, limit: Int): Single<List<SymbolCodeEntity>> {
+        return Single.just(
+            boxSymbolCode.query()
+                .startsWith(SymbolCodeEntity_.code, query)
+                .build()
+                .find(page * limit.toLong(), limit.toLong())
+        )
+    }
+
+    override fun saveSymbols(symbols: SymbolsWrapper): Single<List<SymbolEntity>> {
+        return Single.fromCallable {
+            val symbolsEntities = mutableListOf<SymbolEntity>()
+            symbols.symbolCodes.forEach { symbol ->
+                val symbolEntity = Mapper.symbolToEntity(symbol.value)
+                symbolEntity.code!!.target = getSymbolCode(symbol.key)
+                symbolEntity.company!!.target = SymbolCompanyEntity(symbol.value.companyName)
+                symbolEntity.id = getSymbolByCode(symbol.key)?.id ?: 0L
+
+                boxSymbol.put(symbolEntity)
+                symbolsEntities.add(symbolEntity)
+            }
+
+            symbolsEntities
+        }
+    }
+
+    override fun getSymbolCompany(symbolId: Long): Single<SymbolCompanyEntity> {
+        return Single.just(boxSymbolCompany.get(symbolId))
+    }
+
+    override fun getSymbolNews(symbolId: Long): Single<List<SymbolNewsEntity>> {
+        return Single.just(boxSymbol.get(symbolId).news)
+    }
+
+    override fun isEmpty(): Single<Boolean> {
+        return Single.just(boxSymbolCode.isEmpty)
+    }
+
+    private fun getSymbolCode(code: String) =
+        boxSymbolCode.query()
+            .equal(SymbolCodeEntity_.code, code)
+            .build()
+            .findUnique()
+
+    private fun getSymbolByCode(code: String): SymbolEntity? {
+        val builder = boxSymbol.query()
+        builder.link(SymbolEntity_.code).equal(SymbolCodeEntity_.code, code)
+        return builder.build().findFirst()
+    }
 }
--- app/src/main/java/com/lobster/usb/domain/interfaces/ILocalRepository.kt
+++ app/src/main/java/com/lobster/usb/domain/interfaces/ILocalRepository.kt
@@ -1,4 +1,24 @@
 package com.lobster.usb.domain.interfaces
 
+import com.lobster.usb.domain.entities.SymbolCodeEntity
+import com.lobster.usb.domain.entities.SymbolCompanyEntity
+import com.lobster.usb.domain.entities.SymbolEntity
+import com.lobster.usb.domain.entities.SymbolNewsEntity
+import com.lobster.usb.domain.wrappers.SymbolsWrapper
+import io.reactivex.Single
+
 interface ILocalRepository {
+
+    fun saveSymbolCodes(symbolCodes: List<SymbolCodeEntity>): Single<List<SymbolCodeEntity>>
+
+    fun getSymbolCodes(query: String, page: Int, limit: Int): Single<List<SymbolCodeEntity>>
+
+    fun saveSymbols(symbols: SymbolsWrapper): Single<List<SymbolEntity>>
+
+    fun getSymbolCompany(symbolId: Long): Single<SymbolCompanyEntity>
+
+    fun getSymbolNews(symbolId: Long): Single<List<SymbolNewsEntity>>
+
+    fun isEmpty(): Single<Boolean>
+
 }
